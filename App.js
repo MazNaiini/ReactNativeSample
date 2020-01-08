b80b2d4e1269c2617f7f246fe741929a3c428a53
Dr. Maz Naiini <maz@ma-na.co>
2020-01-08 21:39:55 +0100

    Updated all dependencies to have a navigation stack

--- App.js
+++ App.js
@@ -9,13 +9,13 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {MainList} from './screens/main';
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <MainList />
-    </>
-  );
-};
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+const MainNavigator = createStackNavigator({
+  Welcome: {screen: MainList},
+});
+
+const App = createAppContainer(MainNavigator);
 
 export default App;
