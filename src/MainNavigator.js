2f8611c6904eb7aeb1c72d21520e998e65db99a2
Maz Naiini <maz@ma-na.co>
2020-01-12 09:11:02 +0100

    some linting

--- src/MainNavigator.js
+++ src/MainNavigator.js
@@ -10,9 +10,9 @@ import {APIFetchedList} from './screens/apiFetchedList';
 import {createStackNavigator} from 'react-navigation-stack';
 
 const MainNavigator = createStackNavigator({
-    MainList: {screen: MainList},
-    LocallyLoadedList: {screen: LocallyStoredList},
-    APIFetchedList: {screen: APIFetchedList},
-  });
+  MainList: {screen: MainList},
+  LocallyLoadedList: {screen: LocallyStoredList},
+  APIFetchedList: {screen: APIFetchedList},
+});
 
 export default MainNavigator;
