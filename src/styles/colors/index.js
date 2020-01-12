1dd0dfc364200b97cf2df756f873320cb85c0eeb
Maz Naiini <maz@ma-na.co>
2020-01-12 18:25:01 +0100

    Added two pacakges
    - eslint-import-resolver-babel-module
    - eslint-plugin-import

--- src/styles/colors/index.js
+++ src/styles/colors/index.js
@@ -6,16 +6,14 @@
 'use_strict';
 
 export default {
-    primary: '#1292B4',
-    white: '#FFF',
-    lighter: '#F3F3F3',
-    light: '#DAE1E7',
-    dark: '#444',
-    gray: '#808080',
-    black: '#000',
-    red: '#ff0000',
-    green: '#01aa30',
-    skyBlue: '#87ceeb',
+  primary: '#1292B4',
+  white: '#FFF',
+  lighter: '#F3F3F3',
+  light: '#DAE1E7',
+  dark: '#444',
+  gray: '#808080',
+  black: '#000',
+  red: '#ff0000',
+  green: '#01aa30',
+  skyBlue: '#87ceeb',
 };
-
-  
