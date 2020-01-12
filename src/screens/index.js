379d062a1bad6598b85790ca49f858ed8bb49364
Maz Naiini <maz@ma-na.co>
2020-01-12 14:45:12 +0100

    To use only 'screens' in import

---
+++ src/screens/index.js
@@ -0,0 +1,13 @@
+/**
+ * Index for the main screen
+ * @flow
+ * @format
+ */
+
+'use strict';
+
+import MainList from './main/MainList';
+import APIFetchedList from './apiFetchedList/APIFetchedList';
+import LocallyStoredList from './locallyStoredList/LocallyStoredList';
+
+export {MainList, APIFetchedList, LocallyStoredList};
