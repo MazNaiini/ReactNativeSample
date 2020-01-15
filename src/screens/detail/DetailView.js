0aeae1d099da92f79ed77cceb4672e73bf68408e
Dr. Maz Naiini <maz@ma-na.co>
2020-01-15 23:44:48 +0100

    More work on the detail view

--- src/screens/detail/DetailView.js
+++ src/screens/detail/DetailView.js
@@ -7,7 +7,8 @@
 'use strict';
 
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, StyleSheet, Image, ScrollView} from 'react-native';
+import {Spacer} from '_components';
 
 class DetailView extends React.Component {
   static navigationOptions = {
@@ -16,13 +17,63 @@ class DetailView extends React.Component {
   };
 
   render() {
-    const navigation = this.props.navigation;
     return (
-      <SafeAreaView>
-        <Text>{navigation.state.params.title ?? 'No title'}</Text>
+      <SafeAreaView style={styles.content}>
+        <ScrollView
+          style={styles.scrollView}
+          contentContainerStyle={styles.scrollViewContent}
+          centerContent={true}>
+          {this.renderContent()}
+        </ScrollView>
       </SafeAreaView>
     );
   }
+
+  renderContent() {
+    const params = this.props.navigation.state.params;
+    const title = params.title ?? 'No title';
+    const body = params.body ?? 'No body text';
+    const image = require('_assets/images/logo.png');
+    return (
+      <>
+        <Image resizeMode={'contain'} style={styles.image} source={image} />
+        <Spacer height={30} />
+        <Text style={styles.titleText}>{title}</Text>
+        <Spacer height={40} />
+        <Text style={styles.bodyText}>{body}</Text>
+      </>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  scrollView: {
+    paddingTop: 16,
+  },
+  scrollViewContent: {
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  image: {
+    height: 210,
+    alignSelf: 'center',
+  },
+  titleText: {
+    fontSize: 25,
+    textAlign: 'center',
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  bodyText: {
+    fontSize: 50,
+    textAlign: 'center',
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+});
 
 export default DetailView;
