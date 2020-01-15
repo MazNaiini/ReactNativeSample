7ce7ce3913b610c4047e82c41c3415153cf068a1
Dr. Maz Naiini <maz@ma-na.co>
2020-01-15 23:45:52 +0100

    Some refactoring

--- src/components/Spacer.js
+++ src/components/Spacer.js
@@ -9,11 +9,11 @@
 import React from 'react';
 import {View} from 'react-native';
 
+const defaultHeight = 10;
+
 export default class Spacer extends React.Component {
   render() {
     const height = this.props.height ?? defaultHeight;
     return <View style={{height: height}} />;
   }
 }
-
-const defaultHeight = 10;
